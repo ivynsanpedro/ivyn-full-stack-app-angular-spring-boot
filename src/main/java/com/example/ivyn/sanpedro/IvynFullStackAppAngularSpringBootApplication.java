d12c16281a4285b6c6acbfac269bbc29db5f38b3
Ivyn San Pedro <isanpedro@gmail.com>
2018-12-01 21:57:40 -0500

    Initial commit

---
+++ src/main/java/com/example/ivyn/sanpedro/IvynFullStackAppAngularSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.ivyn.sanpedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvynFullStackAppAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvynFullStackAppAngularSpringBootApplication.class, args);
+	}
+}
